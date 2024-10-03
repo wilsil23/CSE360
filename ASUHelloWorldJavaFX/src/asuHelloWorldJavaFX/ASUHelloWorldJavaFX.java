180462c67442433d645308d79224ff727a846b52
William Diamond <williambernarddiamond@gmail.com>
2024-10-03 15:47:42 -0700

    Third commit, Everything seems to be working!

--- ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/ASUHelloWorldJavaFX.java
+++ ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/ASUHelloWorldJavaFX.java
@@ -14,7 +14,7 @@ public class ASUHelloWorldJavaFX extends Application {
     }
     
     public void start(Stage primaryStage) {
-    	System.out.println("ASU Hello World!dasdsadsadasdasd");
+    	System.out.println("ASU Hello World!");
     	System.out.println("It started!");
         primaryStage.setTitle("ASU Hello World Spring 2024");
         Button btn = new Button();
