c7015ae03541d68eb8c3923f799eb55ac34a305e
19704 <19704@10.70.133.196>
2024-10-04 17:35:14 -0700

    Included linked list start and add function

---
+++ ProjectCSE360/src/UserLinkedList/UserListFunctions.java
@@ -0,0 +1,20 @@
+package UserLinkedList;
+
+public class UserListFunctions{
+	public UserNode head;	//Start of linked list
+	
+	// Method to add to linked list
+	public void addUser(String email, String username, String password, String name, String role) {
+		UserNode newUser = new UserNode(email, username, password, name, role);
+		if (head == null) {
+			head = newUser;
+			newUser.role = "admin";	// First user is admin
+		} else {
+			UserNode current = head;
+			while(current.next != null) {
+				current = head.next;
+			}
+			current.next = newUser;	// Add user to end of list
+		}
+	}
+}
