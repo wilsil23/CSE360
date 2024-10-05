fd9909285f2699db41f6c2a1491c818479661a87
19704 <19704@10.70.133.196>
2024-10-05 11:00:56 -0700

    Adding working remove function

--- ProjectCSE360/src/Main.java
+++ ProjectCSE360/src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         linkedList userList = new linkedList();
         userList.addUser("test@gmail.com", "test", "password","Test", "Student");
         userList.addUser("carson@gmail.com", "cwill228", "password", "Carson", "Student");
+        userList.addUser("test2@gmail.com", "test2", "password","Test", "Student");
+        userList.printUsers();
+        userList.removeByUser("cwill228");
         userList.printUsers();
     }
 }
