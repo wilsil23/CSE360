2101b2486ba211ae25c818c4bb9299750730ea7f
William Diamond <williambernarddiamond@gmail.com>
2024-10-07 13:46:27 -0700

    first edition of rolepage

--- rolepage/src/rolepage/RolePage.java
+++ rolepage/src/rolepage/RolePage.java
@@ -1,8 +1,9 @@
-package passwordEvaluationTestbed;
+package rolepage;
 
 // JavaFX imports needed to support the Graphical User Interface
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -21,18 +22,15 @@ import javafx.stage.Stage;
  * 
  */
 
-public class PasswordEvaluationGUITestbed extends Application {
+public class RolePage extends Application {
 	
 	/** The width of the pop-up window for the user interface */
 	public final static double WINDOW_WIDTH = 500;
 	/** The height of the pop-up window for the user interface */
 	public final static double WINDOW_HEIGHT = 430;
 	
-	/** A temporary object referencing the application's user interface */
-	public UserInterface theGUI;
-	
 	/** The default constructor */
-	public PasswordEvaluationGUITestbed() {
+	public RolePage() {
 	}
 
 	/**********
@@ -47,11 +45,10 @@ public class PasswordEvaluationGUITestbed extends Application {
 	@Override
 	public void start(Stage theStage) throws Exception {
 		
-		theStage.setTitle("loginpage");			// Label the stage (a window)
+		theStage.setTitle("rolepage");			// Label the stage (a window)
 		
 		Pane theRoot = new Pane();							// Create a pane within the window
 		
-		theGUI = new UserInterface(theRoot);				// Create the Graphical User Interface
 		
 		Scene theScene = new Scene(theRoot, WINDOW_WIDTH, WINDOW_HEIGHT);	// Create the scene
 		
@@ -59,6 +56,30 @@ public class PasswordEvaluationGUITestbed extends Application {
 		
 		theStage.show();									// Show the stage to the user
 		
+		Button teacherRoleButton = new Button();
+		
+		Button studentRoleButton = new Button();
+		
+		Button adminRoleButton = new Button();
+		
+		
+		teacherRoleButton.setText("teacher");
+		teacherRoleButton.setLayoutX(200);
+		teacherRoleButton.setLayoutY(300);	
+		teacherRoleButton.setPrefWidth(100);
+		
+		studentRoleButton.setText("student");
+		studentRoleButton.setLayoutX(200);
+		studentRoleButton.setLayoutY(200);	
+		studentRoleButton.setPrefWidth(100);
+		
+		adminRoleButton.setText("admin");
+		adminRoleButton.setLayoutX(200);
+		adminRoleButton.setLayoutY(100);	
+		adminRoleButton.setPrefWidth(100);
+		
+		
+		theRoot.getChildren().addAll(teacherRoleButton, studentRoleButton, adminRoleButton);
 		
 		// When the stage is shown to the user, the pane within the window is visible.  This means
 		// that the labels, fields, and buttons of the Graphical User Interface (GUI) are visible 
